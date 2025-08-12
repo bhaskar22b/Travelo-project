9a2656afcc1afb0fad091dba767c173c6aa9ec3e
bhaskar <bhaskarbhushan22@gmail.com>
2025-08-12 17:34:38 +0530

    change image path

--- src/json.js
+++ src/json.js
@@ -7,79 +7,79 @@ export const navData = {
 }
 
 export const TravelInfo1 = {
-    src: "./public/travel-1.svg",
+    src: "/travel-1.svg",
     title:"Comfortable Journey",
     description:"A wonderful serenity has taken to the possession of my entire soul. "
 }
 
 export const TravelInfo2 = {
-    src: "./public/travel-2.svg",
+    src: "/travel-2.svg",
     title:"Luxuries Hotel",
     description:"A wonderful serenity has taken to the possession of my entire soul. "
 }
 
 export const TravelInfo3 = {
-    src: "./public/travel-3.svg",
+    src: "/travel-3.svg",
     title:"Travel Guide",
     description:"A wonderful serenity has taken to the possession of my entire soul."
 }
 
 export const TripsInfo = {
-    src: "./public/trips-1.webp",
+    src: "/Trips-1.webp",
     title:"Oct 12, 2019",
     description:"Journeys Are Best Measured In New Friends"
 }
 
 export const TripsInfo1 = {
-    src: "./public/trips-2.webp",
+    src: "/Trips-2.webp",
     title:"Oct 12, 2019",
     description:"Journeys Are Best Measured In New Friends"
 }
 
 export const TripsInfo2 = {
-    src: "./public/trips-3.webp",
+    src: "/Trips-3.webp",
     title:"Oct 12, 2019",
     description:"Journeys Are Best Measured In New Friends"
 }
 
 export const DestinationInfo1 ={
-    src: "./public/Destination-1.png",
+    src: "/Destination-1.png",
     title: "Italy",
     places: "07 Places"
 }
 
 export const DestinationInfo2 ={
-    src: "./public/Destination-2.png",
+    src: "/Destination-2.png",
     title: "Brazil ",
     places: "03 Places"
 }
 
 export const DestinationInfo3 ={
-    src: "./public/Destination-3.png",
+    src: "/Destination-3.png",
     title: "America",
     places: "10 Places"
 }
 
 export const DestinationInfo4 ={
-    src: "./public/Destination-4.png",
+    src: "/Destination-4.png",
     title: "Nepal",
     places: "02 Places"
 }
 
 export const DestinationInfo5 ={
-    src: "./public/Destination-5.png",
+    src: "/Destination-5.png",
     title: "Maldives ",
     places: "05 Places"
 }
 
 export const DestinationInfo6 ={
-    src: "./public/Destination-6.png",
+    src: "/Destination-6.png",
     title: "Indonesia ",
     places: "06 Places"
 }
 
 export const PlacesInfo1 ={
-    src: "./public/Places-1.webp",
+    src: "/Places-1.webp",
     placeName: "California",
     cityName: "United State of America",
     rating: "(20 Review)",
@@ -88,7 +88,7 @@ export const PlacesInfo1 ={
 }
 
 export const PlacesInfo2 ={
-    src: "./public/Places-2.webp",
+    src: "/Places-2.webp",
     placeName: "Korola Megna",
     cityName: "United State of America",
     rating: "(20 Review)",
@@ -97,7 +97,7 @@ export const PlacesInfo2 ={
 }
 
 export const PlacesInfo3 ={
-    src: "./public/Places-3.webp",
+    src: "/Places-3.webp",
     placeName: "London",
     cityName: "United State of America",
     rating: "(20 Review)",
@@ -106,7 +106,7 @@ export const PlacesInfo3 ={
 }
 
 export const PlacesInfo4 ={
-    src: "./public/Places-4.webp",
+    src: "/Places-4.webp",
     placeName: "Miami Beach",
     cityName: "United State of America",
     rating: "(20 Review)",
@@ -115,7 +115,7 @@ export const PlacesInfo4 ={
 }
 
 export const PlacesInfo5 ={
-    src: "./public/Places-5.webp",
+    src: "/Places-5.webp",
     placeName: "California",
     cityName: "United State of America",
     rating: "(20 Review)",
@@ -124,7 +124,7 @@ export const PlacesInfo5 ={
 }
 
 export const PlacesInfo6 ={
-    src: "./public/Places-6.webp",
+    src: "/Places-6.webp",
     placeName: "Saintmartine Iceland",
     cityName: "United State of America",
     rating: "(20 Review)",
